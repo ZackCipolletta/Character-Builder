278a37356ac8373980c97fbae09cf0495089f634
Stephen <szook7@gmail.com>
2023-01-12 11:44:25 -0800

    we are stupid

--- js/script.js
+++ js/script.js
@@ -4,18 +4,22 @@ function runAll(event) {
     const characterName = document.getElementById("userName").value;
         console.log(characterName);
 
-   // function handleSelectCharName() {
+   function handleSelectCharName() {
         const charSelection = document.getElementById("height").value;
         console.log(charSelection);
-
+   }
     
     
-   // function handleClassSelection(){
-       //const classSelection = document.querySelector("input[name='class']:checked").value;
-        //console.log(classSelection);
+    function handleClassSelection(){
+       const classSelection = document.querySelector("input[name='class']:checked").value;
+         console.log(classSelection);
     
     event.preventDefault();
 
+    }
+
+    handleClassSelection();
+    handleSelectCharName();
 
 }
 
