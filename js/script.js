59392d551a42dd4b6565f41c9f3bf81d22e423f0
Stephen <szook7@gmail.com>
2023-01-12 13:28:35 -0800

    Updated scripts to hanlde user input for height and class values.
    Co-authored-by: Zachary Cipolletta <ZackCipolletta@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,27 +1,43 @@
 
 //UI Logic
 function runAll(event) {
-    const characterName = document.getElementById("userName").value;
-        console.log(characterName);
-
-   function handleSelectCharName() {
-        const charSelection = document.getElementById("height").value;
-        console.log(charSelection);
-   }
-    
-    
-    function handleClassSelection(){
-       const classSelection = document.querySelector("input[name='class']:checked").value;
-         console.log(classSelection);
-    
     event.preventDefault();
+    
 
+    handleCharName();
+    handleSelectCharHeight();
+    handleClassSelection();
+}
+
+    function handleCharName() {
     }
 
-    handleClassSelection();
-    handleSelectCharName();
+    function handleSelectCharHeight() {
+        const charHeight = document.getElementById("height").value;
+        let race = 'none';
+        if (charHeight === '6'){
+            race = "Elf"
+        } else if (charHeight === '5'){
+            race = "Human"
+        } else if (charHeight === '4'){
+            race = "Dwarf"
+        }
+        return race;
+    }
+    
+    function handleClassSelection(){
+        const classSelection = document.querySelector("input[name='classSelect']:checked").value;
+        let className = 'none';
+        if (classSelection === '1'){
+            className = "Mage"
+        } else if (classSelection === '2'){
+            className = "Knight"
+        } else if (classSelection === '3'){
+            className = "Rogue"
+        }
+        return className;
 
-}
+    }
 
 
 window.addEventListener("load", function() {
