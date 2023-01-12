d901a89375e1492b519a6f32ae895a6bc178a32a
Stephen <szook7@gmail.com>
2023-01-12 11:28:33 -0800

    created basic html for forms and made outline for functions
    Co-authored-by:Zachary Cipolletta <ZackCipolletta@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+//UI Logic
+function runAll(event) {
+    const characterName = document.getElementById("userName").value;
+        console.log(characterName);
+
+   // function handleSelectCharName() {
+        const charSelection = document.getElementById("height").value;
+        console.log(charSelection);
+
+    
+    
+   // function handleClassSelection(){
+       //const classSelection = document.querySelector("input[name='class']:checked").value;
+        //console.log(classSelection);
+    
+    event.preventDefault();
+
+
+}
+
+
+window.addEventListener("load", function() {
+    document.getElementById("whole-form").addEventListener("submit", runAll);
+    
+
+
+});
